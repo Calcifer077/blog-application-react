d3c2ea462097a219baaa51536c84678797a1a06f
Calcifer077 <maheshnashier14@gmail.com>
2025-02-20 18:24:43 +0530

    Added Search functionality

--- Frontend/src/pages/Search.jsx
+++ Frontend/src/pages/Search.jsx
@@ -1,15 +1,73 @@
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import toast from 'react-hot-toast';
+
+import { addBlog, clearBlog } from '../features/Blogs/blogSlice';
+import blogService from '../services/apiBlogs';
+import SearchContent from '../components/SearchContent';
+
 function Search() {
+  const [searchText, setSearchText] = useState('');
+  const [searchBy, setSearchBy] = useState('title');
+  const [searched, setSearched] = useState(false);
+
+  // To dispatch actions of reducers.
+  const dispatch = useDispatch();
+
+  if (searchText !== ' ') {
+    if (searchText.charAt(searchText.length - 1) === ' ') {
+      onSubmit(searchText, searchBy);
+    }
+  }
+
+  async function onSubmit(searchText, searchBy) {
+    try {
+      const res = await blogService.searchBlog(searchText, searchBy);
+
+      dispatch(clearBlog());
+
+      res.forEach((el) => {
+        dispatch(addBlog(el));
+      });
+
+      setSearched(true);
+    } catch (err) {
+      toast.error(err.message || 'Something went wrong! Please try again.');
+    }
+  }
+
   return (
-    <div className="w-full h-screen p-10 flex flex-col items-center bg-gray-100">
-      <h1 className="text-center font-bold text-2xl mb-6">Search here!</h1>
-      <form action="">
-        <input
-          type="text"
-          placeholder="Search"
-          className="w-md p-3 h-10 border-2 rounded-lg focus:outline-none focus:bg-white"
-        />
-      </form>
-    </div>
+    <>
+      <div
+        className={`flex w-full flex-col items-center overflow-auto bg-gray-100 p-10 ${searched ? '' : 'h-screen'}`}
+      >
+        <h1 className="mb-6 text-center text-2xl font-bold">Search here!</h1>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            onSubmit(searchText, searchBy);
+          }}
+          className="flex gap-4"
+        >
+          <input
+            type="text"
+            placeholder="Search"
+            value={searchText}
+            onChange={(e) => setSearchText(e.target.value)}
+            className="h-10 w-md rounded-lg border-2 p-3 focus:bg-white focus:outline-none"
+          />
+          <select
+            value={searchBy}
+            onChange={(e) => setSearchBy(e.target.value)}
+            className="h-10 cursor-pointer rounded-lg border-2 bg-white p-2"
+          >
+            <option value="title">Search By Title</option>
+            <option value="content">Search By Content</option>
+          </select>
+        </form>
+      </div>
+      <div>{searched && <SearchContent />}</div>
+    </>
   );
 }
 
